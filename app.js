fb01eba2a5d957b8ac9a8b98578862dfb19c3d9a
Matt <84022755+Matiss1337@users.noreply.github.com>
2022-11-22 11:42:47 +0100

    added theme and changed font

--- app.js
+++ app.js
@@ -8,16 +8,40 @@ function moveToForm() {
   form.scrollIntoView({behavior: 'smooth'})
 }
 
+function toggleDark() {
+document.documentElement.style.setProperty('--main-color', '#d90429');
+document.documentElement.style.setProperty('--secondary-color', '#bc4749');
+document.documentElement.style.setProperty('--third-color', '#3d405b');
+document.documentElement.style.setProperty('--fourth-color', '#fdfffc');
+sun.style.display = "inline";
+moon.style.display = "none";
+}
+
+function toggleLight() {
+document.documentElement.style.setProperty('--main-color', '#d90429');
+document.documentElement.style.setProperty('--secondary-color', '#f5cac3');
+document.documentElement.style.setProperty('--third-color', '#fdfffc');
+document.documentElement.style.setProperty('--fourth-color', 'black');
+moon.style.display = "inline";
+sun.style.display = "none";
+}
+
 const aboutMe = document.getElementById("aboutMe")
 const aboutMeButton = document.getElementById("aboutMeButton")
 const projects = document.getElementById("projects")
 const projectsButton = document.getElementById("projectstButton")
 const form = document.getElementById("contactForm")
 const formButton = document.getElementById("contactButton")
+const sun = document.getElementById("sun")
+const moon = document.getElementById("moon")
+
+
 
 aboutMeButton.addEventListener("click", moveToAboutMe);
 projectsButton.addEventListener("click", moveToProjects);
 formButton.addEventListener("click", moveToForm);
+sun.addEventListener("click", toggleLight);
+moon.addEventListener("click", toggleDark);
 
 
 
@@ -43,3 +67,5 @@ javascript.addEventListener("mouseleave", () =>
 );
 
 console.log("Hi, im Matt, I love web development.")
+
+
